a6d26d27b7c57fb421ea6fdb8bb76a27e14be076
parekhjatin2017 <parekhjatin2017@gmail.com>
2025-03-05 12:59:17 +0530

    Change - added unit tests for domain

---
+++ domain/src/test/java/com/lbg/domain/GetMovieDetailUseCaseTest.kt
@@ -0,0 +1,52 @@
+package com.lbg.domain
+
+import app.cash.turbine.test
+import com.lbg.domain.model.ApiStatus
+import com.lbg.domain.model.FilmDetails
+import com.lbg.domain.model.Resource
+import com.lbg.domain.repository.IMoviesRepository
+import com.lbg.domain.usecases.GetMovieDetailUseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.Mockito
+
+@ExperimentalCoroutinesApi
+class GetMovieDetailUseCaseTest {
+
+    private val repository = Mockito.mock(IMoviesRepository::class.java)
+    private val id = "5"
+
+    @Test
+    fun testMovieDetails() {
+        runTest {
+            val detail = Mockito.mock(FilmDetails::class.java, Mockito.RETURNS_DEEP_STUBS)
+            Mockito.doReturn(Resource.Success(detail)).`when`(repository)
+                .getMovieDetail(anyString(), anyString())
+
+            GetMovieDetailUseCase(repository)(id).test {
+                assert(ApiStatus.Loading == awaitItem())
+                assert(
+                    ApiStatus.MovieDetailSuccess(detail).javaClass
+                            == awaitItem().javaClass
+                )
+                cancelAndIgnoreRemainingEvents()
+            }
+        }
+    }
+
+    @Test
+    fun negativeTestMovieDetails() {
+        runTest {
+            Mockito.doReturn(Resource.Error<FilmDetails>("")).`when`(repository)
+                .getMovieDetail(anyString(), anyString())
+
+            GetMovieDetailUseCase(repository)(id).test {
+                assert(ApiStatus.Loading == awaitItem())
+                assert(ApiStatus.Error("").javaClass == awaitItem().javaClass)
+                cancelAndIgnoreRemainingEvents()
+            }
+        }
+    }
+}
---
+++ domain/src/test/java/com/lbg/domain/GetPopularMoviesUseCaseTest.kt
@@ -0,0 +1,51 @@
+package com.lbg.domain
+
+import app.cash.turbine.test
+import com.lbg.domain.model.ApiStatus
+import com.lbg.domain.model.Film
+import com.lbg.domain.model.Resource
+import com.lbg.domain.repository.IMoviesRepository
+import com.lbg.domain.usecases.GetPopularMoviesUseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.Mockito
+
+@ExperimentalCoroutinesApi
+class GetPopularMoviesUseCaseTest {
+
+    private val repository = Mockito.mock(IMoviesRepository::class.java)
+
+    @Test
+    fun testMovieList() {
+        runTest {
+            val list = listOf(Mockito.mock(Film::class.java))
+            Mockito.doReturn(Resource.Success(list)).`when`(repository)
+                .getPopularMovies(anyString())
+
+            GetPopularMoviesUseCase(repository)().test {
+                assert(ApiStatus.Loading == awaitItem())
+                assert(
+                    ApiStatus.MovieListSuccess(list).javaClass
+                            == awaitItem().javaClass
+                )
+                cancelAndIgnoreRemainingEvents()
+            }
+        }
+    }
+
+    @Test
+    fun negativeTestMovieList() {
+        runTest {
+            Mockito.doReturn(Resource.Error<List<Film>>("")).`when`(repository)
+                .getPopularMovies(anyString())
+
+            GetPopularMoviesUseCase(repository)().test {
+                assert(ApiStatus.Loading == awaitItem())
+                assert(ApiStatus.Error("").javaClass == awaitItem().javaClass)
+                cancelAndIgnoreRemainingEvents()
+            }
+        }
+    }
+}
